580911d1ba53d7d22ba4b5c415dbe1c05dabae87
Sam Hokin <shokin@ncgr.org>
2020-11-11 07:47:50 -0700

    Mods to support Google auth, plus new mines.

---
+++ intermine-privacy-policy_files/readthedocs-data.js
@@ -0,0 +1,22 @@
+var READTHEDOCS_DATA = {
+    project: "intermine",
+    version: "latest",
+    language: "en",
+    programming_language: "java",
+    subprojects: {},
+    canonical_url: "https://intermine.readthedocs.io/en/latest/",
+    theme: "intermine",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    
+    proxied_api_host: "/_",
+    
+    commit: "39405744",
+    ad_free: false,
+
+    global_analytics_code: "UA-17997319-1",
+    user_analytics_code: "UA-1566492-7"
+};
+
